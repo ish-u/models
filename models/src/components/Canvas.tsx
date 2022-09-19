f1c1383640c53188beae9bd03e0bf78bec0dc88e
ish-u <anmolgupta520@gmail.com>
2022-09-20 02:40:01 +0530

    added helpers; resize,load method;

--- models/src/components/Canvas.tsx
+++ models/src/components/Canvas.tsx
@@ -2,47 +2,54 @@ import { useCallback, useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import Controls from "./Controls";
+
+import URL0 from "../assets/Black XBOX controller.glb?url";
+import URL1 from "../assets/P1.glb?url";
+import URL2 from "../assets/2CylinderEngine.glb?url";
 
 export const Canvas = () => {
   const canvas = useRef<HTMLCanvasElement | null>(null);
   const [scene, setScene] = useState<THREE.Scene | null>(null);
-  const [camera, setCamera] = useState<THREE.Camera | null>(null);
+  const [camera, setCamera] = useState<THREE.PerspectiveCamera | null>(null);
   const [renderer, setRenderer] = useState<THREE.WebGLRenderer | null>(null);
+  const [loader, setLoader] = useState<GLTFLoader | null>(null);
+  const [URL, setURL] = useState(URL0);
 
-  //helpers toggle
-  const toggleGrid = () => {
-    if (scene) {
-      const objects = scene?.children;
-      console.log(objects.length);
-      objects.map((o) => {
-        if (o.type === "GridHelper") {
-          o.visible = !o.visible;
-        }
-      });
-      
+  const load = () => {
+    if (loader && camera && renderer && scene) {
+      loader.load(
+        URL,
+        (gltf) => {
+          gltf.scene.scale.set(10, 10, 10);
+          scene.add(gltf.scene);
+        },
+        (progress) => console.log((progress.loaded / progress.total) * 100),
+        (err) => console.error(err)
+      );
     }
   };
 
   const animate = () => {
     if (camera && scene && renderer) {
       console.log("animate");
-
-      const geometry = new THREE.BoxGeometry(1, 1, 1);
-      const material = new THREE.MeshBasicMaterial({ color: 0x00f0f0 });
-      const cube = new THREE.Mesh(geometry, material);
-      scene?.add(cube);
-      camera.position.z = 5;
-
+      load();
       function loop() {
         requestAnimationFrame(loop);
         if (scene && camera && renderer) {
           //   console.log("loop");
-          //   cube.rotation.x += 0.01;
-          //   cube.rotation.y += 0.01;
           renderer.render(scene, camera);
         }
       }
       loop();
+    }
+  };
+
+  const resize = () => {
+    if (camera && scene && renderer) {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
     }
   };
 
@@ -52,11 +59,14 @@ export const Canvas = () => {
 
     // CAMERA
     const newCamera = new THREE.PerspectiveCamera(
-      90,
+      75,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
     );
+    newCamera.position.x = 2;
+    newCamera.position.y = 2;
+    newCamera.position.z = 2;
 
     // RENDERER
     const newRenderer = new THREE.WebGLRenderer({
@@ -66,14 +76,34 @@ export const Canvas = () => {
     newRenderer.setPixelRatio(window.devicePixelRatio);
     newRenderer.setClearColor(0x0f0f0f, 1);
 
+    // GLTF LOADER
+    const gltfLoader = new GLTFLoader();
+    setLoader(gltfLoader);
+
     // Adding the Camera
     newScene.add(newCamera);
+
+    // Adding Lighting
+    const light = new THREE.PointLight(0xffffff, 10);
+    light.position.set(10, 10, 10);
+    newScene.add(light);
+
+    const ambLight = new THREE.AmbientLight(0x404040); // soft white light
+    newScene.add(light);
 
     // Adding orbit controls
     const orbit = new OrbitControls(newCamera, newRenderer.domElement);
 
+    // helpers
+    // GRID
     const grid = new THREE.GridHelper(1000, 1000);
     newScene.add(grid);
+    // AXES
+    const axesHelper = new THREE.AxesHelper(1000);
+    newScene.add(axesHelper);
+    // LIGHT
+    const pointLightHelper = new THREE.PointLightHelper(light, 5);
+    newScene.add(pointLightHelper);
 
     setScene(newScene);
     setCamera(newCamera);
@@ -86,17 +116,17 @@ export const Canvas = () => {
 
   useEffect(() => {
     animate();
+    window.addEventListener("resize", resize);
+    return () => {
+      window.removeEventListener("resize", resize);
+    };
   }, [scene, camera, renderer]);
+
+  useEffect(() => {}, []);
 
   return (
     <>
-      <button
-        className="fixed top-4 left-4 p-2 m-2 z-10 text-white text-lg font-bold
-                 bg-emerald-500/50 hover:bg-emerald-500 transition duration-300 border border-emerald-400 rounded-md"
-        onClick={toggleGrid}
-      >
-        Toggle Grid
-      </button>
+      {scene && <Controls scene={scene} />}
       <canvas ref={canvas} className="fixed top-0 left-0 -z-50"></canvas>;
     </>
   );
