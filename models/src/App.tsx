61fbc535f0821cfb052ffbe31aabfeea232dfc80
ish-u <anmolgupta520@gmail.com>
2022-09-21 00:25:26 +0530

    moved getFiles to Canvas

--- models/src/App.tsx
+++ models/src/App.tsx
@@ -2,25 +2,7 @@ import { useEffect, useState } from "react";
 import Canvas from "./components/Canvas";
 
 function App() {
-  const [files, setFiles] = useState([]);
-
-  useEffect(() => {
-    const getFiles = async () => {
-      try {
-        const res = await fetch("http://localhost:5000/files");
-        const data = await res.json();
-        setFiles(data.files);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    if (!files.length) {
-      getFiles();
-    }
-  }, []);
-
-  return <Canvas files={files} />;
+  return <Canvas />;
 }
 
 export default App;
