e58b08267480e8cd72a2767eaf1415196fa67797
ish-u <anmolgupta520@gmail.com>
2022-09-22 00:29:58 +0530

    added passport-local strategy

---
+++ server/src/config/session.ts
@@ -0,0 +1,43 @@
+import passport from "passport";
+import PassportLocal from "passport-local";
+import UserModel from "../models/user.model";
+import bcrypt from "bcrypt";
+
+// Implementing Strategy
+passport.use(
+  new PassportLocal.Strategy(async (username, password, cb) => {
+    try {
+      console.log(username, password);
+      const doc = await UserModel.findOne({ username: username });
+      if (!doc) {
+        return cb(null, false);
+      }
+      const isValid = await bcrypt.compare(password, doc.password);
+      if (isValid) {
+        return cb(null, doc.id);
+      } else {
+        return cb(null, false);
+      }
+    } catch (err) {
+      console.error(err);
+      cb(err);
+    }
+  })
+);
+
+// Passport Helpers
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(async function (id, cb) {
+  try {
+    const doc = await UserModel.findById(id);
+    if (!doc) {
+      return cb("ERROR");
+    }
+    cb(null, doc);
+  } catch (err) {
+    console.error(err);
+  }
+});
