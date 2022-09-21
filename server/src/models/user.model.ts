d628d50eaad4a86a271c90e2631bc23f18963da7
ish-u <anmolgupta520@gmail.com>
2022-09-22 00:32:12 +0530

    added User model

---
+++ server/src/models/user.model.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+// User Schema
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("User", UserSchema);
