4465d95ff47dead518f785e1822d97f0fc1933e5
ish-u <anmolgupta520@gmail.com>
2022-09-22 00:30:34 +0530

    added passport-local strategy

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import { Express } from "express";
 import cors from "cors";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import passport from "passport";
+import "./config/session";
 
 // ENV Variables
 import dotenv from "dotenv";
+import { authenticate } from "./utils/authenticate";
 dotenv.config();
 
 // Express App
@@ -13,8 +18,27 @@ const app: Express = express();
 app.use(
   cors({
     origin: ["http://localhost:5173", process.env.FRONTEND || ""],
+    credentials: true,
   })
 );
-app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.DB_URI || "",
+    }),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+// app.use("/public", [authenticate, express.static("public")]);
+app.use("/public", express.static("public"));
 
 export default app;
