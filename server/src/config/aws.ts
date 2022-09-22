bd6dbd1ea32cecead070bd0e105c949acd03debb
ish-u <anmolgupta520@gmail.com>
2022-09-22 15:57:51 +0530

    added s3 config

---
+++ server/src/config/aws.ts
@@ -0,0 +1,10 @@
+import { S3Client } from "@aws-sdk/client-s3";
+
+// S3
+export const S3 = new S3Client({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+  },
+});
